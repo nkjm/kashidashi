7b46b848732dce7996c67e9eb6c7b7224058c7ec
Kazuki Nakajima <nkjm@nkjm-Air.HRTW3>
2017-07-10 15:42:54 +0900

    test

--- index.js
+++ index.js
@@ -13,7 +13,30 @@ const LINE_CONFIG = {
 
 server.use(line.middleware(LINE_CONFIG)); // 追加
 
-server.post("/webhook", (req, res, next) => {
+// APIコールのためのクライアントインスタンスを作成
+let bot = new line.Client(LINE_CONFIG);
+
+// -----------------------------------------------------------------------------
+// ルーター設定
+server.post('/webhook', (req, res, next) => {
+    // 先行してLINE側にステータスコード200でレスポンスする。
     res.send(200);
-    console.log(req.body);
+
+    // すべてのイベント処理のプロミスを格納する配列。
+    let events_processed = [];
+
+    // イベントオブジェクトを順次処理。
+    for (let event of req.body.events){
+        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+        if (event.type == "message" || event.message.type == "text"){
+            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+            if (event.message.text == "こんにちは"){
+                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "これはこれは"
+                });
+            }
+        }
+    };
 });
