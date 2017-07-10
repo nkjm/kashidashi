1eff3b47376a7792aa89a61de442003505046fd5
Kazuki Nakajima <nkjm@nkjm-Air.HRTW3>
2017-07-10 15:22:34 +0900

    test

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 let restify = require("restify");
+let line = require("@line/bot-sdk"); // 追加
 
 let server = restify.createServer();
 server.listen(process.env.PORT || 3000, function() {
     console.log("Node is running...");
 });
 
-server.get("/", (req, res, next) => {
-    res.send({body: "Hello World"});
+const LINE_CONFIG = {
+    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_CHANNEL_SECRET
+}
+
+server.use(line.middleware(LINE_CONFIG)); // 追加
+
+server.post("/webhook", (req, res, next) => {
+    res.send(200);
+    console.log(req.body);
 });
