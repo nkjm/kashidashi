cd6fadb401bb826a3cb83386b276ff11ff9b9fb7
Kazuki Nakajima <nkjm@nkjm-Air.HRTW3>
2017-07-12 16:18:01 +0900

    test

--- service/oracle.js
+++ service/oracle.js
@@ -69,6 +69,7 @@ module.exports = class ServiceOracle {
         let headers = {
             "Content-Type": "application/json"
         }
+        console.log(url);
         return request.getAsync({
             url: url,
             headers: headers,
