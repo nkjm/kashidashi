41c528d002338cf777cedc6070a4e8a48344e790
Kazuki Nakajima <nkjm@nkjm-Air.HRTW3>
2017-07-12 16:06:43 +0900

    test

---
+++ service/oracle.js
@@ -0,0 +1,104 @@
+"use strict";
+
+let request = require("request");
+Promise = require("bluebird");
+const URL_BASE = "https://apex.oracle.com/pls/apex/" + process.env.ORACLE_WORKSPACE + "/kasidasi/" + process.env.ORACLE_ACCESS_TOKEN;
+
+Promise.promisifyAll(request);
+
+module.exports = class ServiceOracle {
+    static toggle_leng_flag(serial){
+        let url = URL_BASE + "/product/toggle/" + encodeURIComponent(serial);
+        let headers = {
+            "Content-Type": "application/json"
+        }
+        return request.putAsync({
+            url: url,
+            headers: headers,
+            json: true
+        }).then(
+            (response) => {
+                if (response.statusCode != 200){
+                    return Promise.reject(new Error("ServiceOracle.toggle_lend_flag() failed."));
+                }
+                return response;
+            }
+        );
+    }
+
+    static get_product_list(){
+        let url = URL_BASE + "/product/list";
+        let headers = {
+            "Content-Type": "application/json"
+        }
+        return request.getAsync({
+            url: url,
+            headers: headers,
+            json: true
+        }).then(
+            (response) => {
+                if (response.statusCode != 200){
+                    return Promise.reject(new Error("ServiceOracle.get_product_list() failed."));
+                }
+                return response.body.items;
+            }
+        );
+    }
+
+    static search_product(key){
+        let url = URL_BASE + "/product/search?key=" + encodeURIComponent(key);
+        let headers = {
+            "Content-Type": "application/json"
+        }
+        return request.getAsync({
+            url: url,
+            headers: headers,
+            json: true
+        }).then(
+            (response) => {
+                if (response.statusCode != 200){
+                    return Promise.reject(new Error("ServiceOracle.search_product() failed."));
+                }
+                return response.body.items;
+            }
+        );
+    }
+
+    static get_product_by_id(id){
+        let url = URL_BASE + "/product?id=" + encodeURIComponent(id);
+        let headers = {
+            "Content-Type": "application/json"
+        }
+        return request.getAsync({
+            url: url,
+            headers: headers,
+            json: true
+        }).then(
+            (response) => {
+                if (response.statusCode != 200){
+                    return Promise.reject(new Error("ServiceOracle.get_product_by_id() failed."));
+                }
+                return response.body.items;
+            }
+        );
+    }
+
+    static get_product_by_name(name){
+        let url = URL_BASE + "/product?name=" + encodeURIComponent(name);
+        let headers = {
+            "Content-Type": "application/json"
+        }
+        return request.getAsync({
+            url: url,
+            headers: headers,
+            json: true
+        }).then(
+            (response) => {
+                if (response.statusCode != 200){
+                    return Promise.reject(new Error("ServiceOracle.get_product_by_name() failed."));
+                }
+                return response.body.items;
+            }
+        );
+    }
+}
---
+++ skill/kasidasi.js
@@ -0,0 +1,178 @@
+"use strict";
+
+let request = require("request");
+Promise = require("bluebird");
+let oracle = require("../service/oracle");
+
+module.exports = class SkillKasidasi {
+    constructor(bot, event){
+        this.optional_parameter = {
+            product_id_or_name: {
+                message_to_confirm: {
+                    type: "text",
+                    text: "品番または品名を教えてください"
+                },
+                reaction: (error, value, context, resolve, reject) => {
+                    if (error){
+                        bot.change_message_to_confirm({
+                            type: "text",
+                            text: "お探しの機器はそもそも存在しないようです。品番または品名をお確かめの上再度教えてください。"
+                        });
+                    }
+                    return resolve();
+                }
+            },
+            product_id: {
+                message_to_confirm: {
+
+                }
+            },
+            product_name: {
+                message_to_confirm: {
+
+                }
+            },
+            lend: {
+                message_to_confirm: {
+                    type: "template",
+                    altText: `こちらの機器を借りられますか？`,
+                    template: {
+                        type: "confirm",
+                        text: `こちらの機器を借りられますか？`,
+                        actions: [
+                            {type:"message", label:"はい", text:"はい"},
+                            {type:"message", label:"いいえ", text:"いいえ"}
+                        ]
+                    }
+                },
+                reaction: (error, value, context, resolve, reject) => {
+                    if (!error){
+                        if (value == "いいえ"){
+                            bot.queue({
+                                type: "text",
+                                text: "だったら聞く必要ないがや。"
+                            })
+                        }
+                    }
+                    return resolve();
+                }
+            }
+        }
+
+        this.clear_context_on_finish = true;
+    }
+
+    parse_product_id_or_name(value, context, resolve, reject){
+        return oracle.get_product_by_id(value).then(
+            (response) => {
+                if (response.length > 0){
+                    return resolve(value);
+                } else {
+                    return oracle.get_product_by_name(value).then(
+                        (response) => {
+                            if (response.length > 0){
+                                return resolve(value);
+                            } else {
+                                return reject();
+                            }
+                        }
+                    );
+                }
+            }
+        );
+    }
+
+    parse_product_id(value, context, resolve, reject){
+        return oracle.get_product_by_id(value).then(
+            (response) => {
+                if (response.length > 0){
+                    return resolve(value);
+                } else {
+                    return reject();
+                }
+            }
+        );
+    }
+
+    parse_product_name(value, context, resolve, reject){
+        return oracle.get_product_by_name(value).then(
+            (response) => {
+                if (response.length > 0){
+                    return resolve(value);
+                } else {
+                    return reject();
+                }
+            }
+        );
+    }
+
+    finish(bot, event, cotext, resolve, reject){
+        // User already specified product and confirmed if he/she wants to get it.
+        if (context.confirmed.lend){
+            if (context.confirmed.lend == "はい"){
+                return oracle.toggle_lend_flag(context.confirmed.product_serial).then(
+                    (response) => {
+                        return bot.reply({
+                            type: "text",
+                            text: "了解しました。貸出処理完了です。持ってってくださいー。"
+                        });
+                    },
+                    (error) => {
+                        return bot.reply({
+                            type: "text",
+                            text: "すみません貸出処理中にエラーが出ました。今日はちょっと無理そうです。"
+                        });
+                    }
+                }).then(
+                    (response) => {
+                        return resolve();
+                    }
+                );
+            } else {
+                return bot.reply().then(
+                    (response) => {
+                        return resolve();
+                    }
+                )
+            }
+        }
+
+        // User has not provided any product information.
+        if (!context.confirmed.product_id_or_name && !context.confirmed.product_id && !context.confirmed.product_name){
+            bot.collect("product");
+            return resolve();
+        }
+
+        // User has provided product information so we search for it.
+        return oracle.search_product(context.confirmed.product_id || context.confirmed.product_name).then(
+            (response) => {
+                if (response.length == 1){
+                    // Available product FOUND.
+                    context.confirmed.product_serial = response[0].t_field1;
+                    bot.collect("lend");
+                    return resolve();
+                }
+                if (response.length == 0){
+                    // Available product NOT FOUND.
+                    let message_text = "残念ながら今";
+                    return bot.reply({
+                        type: "template",
+                        altText: message_text,
+                        template: {
+                            type: "buttons",
+                            text: message_text,
+                            actions: [
+                                {type:"postback", label:"連絡して欲しい", data: `${}が返ってきたら連絡ください`},
+                                {type:"postback", label:"別に必要ない", data: `${}が返ってきても連絡はいりません`}
+                            ]
+                        }
+                    }).then(
+                        (response) => {
+                            return resolve();
+                        }
+                    );
+                }
+            }
+        )
+    }
+}
